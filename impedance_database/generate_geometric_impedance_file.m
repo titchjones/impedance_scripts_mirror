55d4708175baac519ae7743070abb8e573768aff
Teresia Olsson <teresia.olsson@diamond.ac.uk>
2021-05-10 13:07:43 +0100

    Corrected wrong number for planes.

--- impedance_database/generate_geometric_impedance_file.m
+++ impedance_database/generate_geometric_impedance_file.m
@@ -1,6 +1,7 @@
 function generate_geometric_impedance_file(IMPRING,output_filename)
 %% Generate geometric impedance file from impedance database
 % Format: plane length path to wake file path to impedance file
+% Plane: 0 = lon, 1 = hor, 2 = ver
 
     fileID = fopen(output_filename,'w');
 
@@ -8,13 +9,13 @@ function generate_geometric_impedance_file(IMPRING,output_filename)
 
         if ~isempty(IMPRING{i}.Geom_wake_files) && ~isempty(IMPRING{i}.Geom_impedance_files)
 
-            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.Geom_wake_files{1},IMPRING{i}.Geom_impedance_files{1});
-            fprintf(fileID,'1 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.Geom_wake_files{2},IMPRING{i}.Geom_impedance_files{2});
-            fprintf(fileID,'2 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.Geom_wake_files{3},IMPRING{i}.Geom_impedance_files{3}); 
+            fprintf(fileID,'1 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.Geom_wake_files{1},IMPRING{i}.Geom_impedance_files{1});
+            fprintf(fileID,'2 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.Geom_wake_files{2},IMPRING{i}.Geom_impedance_files{2});
+            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.Geom_wake_files{3},IMPRING{i}.Geom_impedance_files{3}); 
         else
-            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,'','');
             fprintf(fileID,'1 %.15f %s %s\n',IMPRING{i}.Length,'','');
             fprintf(fileID,'2 %.15f %s %s\n',IMPRING{i}.Length,'','');
+            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,'','');
         end
 
     end
--- impedance_database/generate_resistive_wall_file.m
+++ impedance_database/generate_resistive_wall_file.m
@@ -10,13 +10,13 @@ function generate_resistive_wall_file(IMPRING,output_filename)
 
         if ~isempty(IMPRING{i}.RW_wake_files) && ~isempty(IMPRING{i}.RW_impedance_files)
 
-            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.RW_wake_files{1},IMPRING{i}.RW_impedance_files{1});
-            fprintf(fileID,'1 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.RW_wake_files{2},IMPRING{i}.RW_impedance_files{2});
-            fprintf(fileID,'2 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.RW_wake_files{3},IMPRING{i}.RW_impedance_files{3});
+            fprintf(fileID,'1 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.RW_wake_files{1},IMPRING{i}.RW_impedance_files{1});
+            fprintf(fileID,'2 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.RW_wake_files{2},IMPRING{i}.RW_impedance_files{2});
+            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,IMPRING{i}.RW_wake_files{3},IMPRING{i}.RW_impedance_files{3});
         else
-            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,'','');
             fprintf(fileID,'1 %.15f %s %s\n',IMPRING{i}.Length,'','');
             fprintf(fileID,'2 %.15f %s %s\n',IMPRING{i}.Length,'','');
+            fprintf(fileID,'0 %.15f %s %s\n',IMPRING{i}.Length,'','');
         end
 
     end
